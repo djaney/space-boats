570617e938f3b7c773517c8f1f69ace7c994b3a5
Djane Rey Mabelin <d.mabelin@arcanys.com>
2015-12-02 16:48:38 +0800

    add FPS counter

--- src/js/game.js
+++ src/js/game.js
@@ -3,15 +3,19 @@
 	var ns = window['space-boats'];
 	function Game() {
 		this.worldSize = 1000;
+		this.cameraPos = new Phaser.Point(0, 0);
 	}
 
 	// Load images and sounds
 	Game.prototype.preload = function() {
+		this.game.time.advancedTiming = true;
 	    this.game.load.spritesheet('ship', '/assets/ship.png', 32, 32);
 	};
 
 	// Setup the example
 	Game.prototype.create = function() {
+
+
 	    // Set stage background color
 	    this.game.stage.backgroundColor = 0x333333;
 		// create player
@@ -23,24 +27,21 @@
 		this.player.spr.anchor.setTo(0.5, 0.5);
 	    this.player.spr.angle = -90; // Point the ship up
 
-
-		// this.game.camera.deadzone = new Phaser.Rectangle(this.game.camera.width,this.game.camera.height,1,1);
-		this.game.camera.follow(this.player.spr,Phaser.Camera.FOLLOW_LOCKON);
-
+		this.game.camera.follow(this.player.spr);
 	};
 
 	// The update() method is called every frame
 	Game.prototype.update = function() {
 		// watch keys
 		this.controls.update();
-		console.log(this.player);
 		this.player.update();
 		this.game.world.setBounds((this.worldSize/2*-1) + this.player.spr.x, (this.worldSize/2*-1) + this.player.spr.y, this.worldSize, this.worldSize);
+
+
 	};
 
 	Game.prototype.render = function(){
-		this.game.debug.spriteCoords(this.player.spr, 32, 500);
-		this.game.debug.cameraInfo(this.game.camera, 32, 32);
+		this.game.debug.text(this.game.time.fps || '--', 2, 14, "#00ff00");
 	};
 
 
