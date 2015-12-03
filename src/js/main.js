5843ebebf6dc356f79fd732fcac240327be2b80e
Djane Rey Mabelin <thedjaney@gmail.com>
2015-12-03 22:44:00 +0800

    add facebook login

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,15 @@
-window.addEventListener('load', function () {
+var loadGame = function (user) {
 	'use strict';
 
 	var ns = window['space-boats'];
 	var game = new Phaser.Game(640, 480, Phaser.AUTO, 'space-boats-game');
-	
+
 	game.state.add('boot', ns.Boot);
 	game.state.add('preloader', ns.Preloader);
 	game.state.add('menu', ns.Menu);
 	game.state.add('game', ns.Game);
 	/* yo phaser:state new-state-files-put-here */
-	game.state.start('boot');
-}, false);
+	game.state.start('game',true,true,{
+		user:user
+	});
+};
