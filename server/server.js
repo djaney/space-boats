5b2b2c4e6dfa501dcb10b8267949326e11806d02
Djane Rey Mabelin <d.mabelin@arcanys.com>
2015-12-04 16:31:52 +0800

    add server start log

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
-
+var port = 9000;
 app.use(express.static(__dirname + '/../src'));
-app.listen(9000);
+app.listen(port);
+console.log('Listening at port: '+ port);
