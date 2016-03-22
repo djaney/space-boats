31260ddc37fcd1eac77ea8cc4083cbc63fbb3bf0
Djane Rey Mabelin <d.mabelin@arcanys.com>
2016-03-22 12:57:35 +0800

    fix redis

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var io = require('socket.io')(http);
 var redis = require('socket.io-redis');
 var util = require('./utils.js');
 var port = process.env.PORT || 9000;
-var redisUrl = process.env.REDIS_URL || 'redis://h:p4jepfe1i57arnftef5tdc40uub@ec2-54-83-207-91.compute-1.amazonaws.com:12279';
+var redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
 
 var Warp = require('./lib/warp');
 var System = require('./lib/system');
